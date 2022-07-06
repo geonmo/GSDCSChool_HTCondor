374f82cbc6d309ee22dc3ce8961f65287ef5259e
Geonmo Ryu <geonmo@kisti.re.kr>
2022-07-06 09:45:59 +0000

    sync account utils.

---
+++ utils/sync_account.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$#" -eq 0 ]; then
+    echo "Sync account information for node1,node2,node3"
+    for num in {1..3}
+    do
+        sudo scp /etc/passwd root@node${num}:/etc/passwd
+        sudo scp /etc/shadow root@node${num}:/etc/shadow
+    done
+
+elif [ "$#" -eq 1 ]; then
+    echo "Sync account information for node1,node2,node3.$1"
+    for num in {1..3}
+    do
+        sudo scp /etc/passwd root@node${num}.$1:/etc/passwd
+        sudo scp /etc/shadow root@node${num}.$1:/etc/shadow
+    done
+else 
+    echo "wrong argv"
+fi
+
+
