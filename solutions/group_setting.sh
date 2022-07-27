9f6f7b29b9b439cc1d8262d74289d6e2b859b25d
Geonmo Ryu <geonmo@kisti.re.kr>
2022-07-27 09:27:12 +0900

    group 환경설정 세팅

---
+++ solutions/group_setting.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+re='^[0-9]$'
+if [ $# -ne 1 ] ; then
+  echo "Wrong argument" >&2
+  exit 1
+elif ! [[ $1 =~ $re ]]; then
+    echo "Error : $1 is not a number" >&2
+    exit 2
+else
+  echo "Group number is $1"
+  sed -i "s/node\([0-9]\).g[0-9].gsdc.org/node\1.g$1.gsdc.org/g" ansible_setup/inventory/condor_hosts  
+  sed -i "s/node\([0-9]\).g[0-9].gsdc.org/node\1.g$1.gsdc.org/g" ansible_setup/vars.yml  
+fi
