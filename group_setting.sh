325e91d416f4991b43e4b83cc83729a72a709358
Geonmo Ryu <geonmo@kisti.re.kr>
2022-07-29 10:09:19 +0900

    chapter 4-1 테스트

--- group_setting.sh
+++ group_setting.sh
@@ -11,4 +11,5 @@ else
   echo "Group number is $1"
   sed -i "s/node\([0-9]\).g[0-9].gsdc.org/node\1.g$1.gsdc.org/g" ansible_setup/inventory/condor_hosts  
   sed -i "s/node\([0-9]\).g[0-9].gsdc.org/node\1.g$1.gsdc.org/g" ansible_setup/vars.yml  
+  sed -i "s/node\([0-9]\).g[0-9].gsdc.org/node\1.g$1.gsdc.org/g" tutorials/chapter4_Advanced/4-1/myinfo.jds 
 fi
--- tutorials/chapter4_Advanced/4-1/nfs_setup.sh
+++ tutorials/chapter4_Advanced/4-1/nfs_setup.sh
@@ -3,3 +3,4 @@ ansible-playbook config_nfs.yml -u root -k
 echo "사용자 디렉토리 생성 : /shared/user/$USER"
 mkdir /shared/user/$USER
 chmod 770 /shared/user/$USER
+cp myinfo* /shared/user/$USER
