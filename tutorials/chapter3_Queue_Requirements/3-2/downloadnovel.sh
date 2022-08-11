2351932cbdc1dd34ba9286acdd0c41e247075b8d
Geonmo Ryu <geonmo@kisti.re.kr>
2022-08-11 11:39:05 +0900

    Update downloadnovel.sh

--- tutorials/chapter3_Queue_Requirements/3-2/downloadnovel.sh
+++ tutorials/chapter3_Queue_Requirements/3-2/downloadnovel.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-echo "password is tut3"
-scp -P4280 tut3@10.10.10.56:/shared/data/novel/*.txt .
+echo "소설을 다운로드 합니다."
+wget http://www.gutenberg.org/cache/epub/31547/pg31547.txt
+wget http://www.gutenberg.org/cache/epub/41481/pg41481.txt
+wget http://www.gutenberg.org/cache/epub/28617/pg28617.txt
+wget http://www.gutenberg.org/cache/epub/29607/pg29607.txt
 
