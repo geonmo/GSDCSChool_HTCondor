5979502d839a3c377f2f27466ef8ff41e1cf3216
Geonmo Ryu <geonmo@kisti.re.kr>
2023-07-27 16:41:40 +0900

    스쿨게이트웨이 정보 추가

--- group_setting.sh
+++ group_setting.sh
@@ -12,4 +12,12 @@ else
   sed -i "s/node\([0-9]\).g[0-9].23.gsdc.org/node\1.g$1.23.gsdc.org/g" ansible_setup/inventory/condor_hosts  
   sed -i "s/node\([0-9]\).g[0-9].23.gsdc.org/node\1.g$1.23.gsdc.org/g" ansible_setup/vars.yml  
   sed -i "s/node\([0-9]\).g[0-9].23.gsdc.org/node\1.g$1.23.gsdc.org/g" tutorials/chapter4_Advanced/4-1/myinfo.jds 
+
+  if [[ $# -le 3 ]]; then
+    echo "school gateway 1"
+    sed -i "s/nfs_server :/nfs_server : \"10.10.10.226\"/g" ansible_setup/vars.yml
+  else
+    echo "schoole gateway 2"
+    sed -i "s/nfs_server :/nfs_server : \"10.10.10.230\"/g" ansible_setup/vars.yml
+  fi
 fi
