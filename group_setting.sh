955fe2a137d36056fc23959a7243c1307f92460a
Geonmo Ryu <geonmo@kisti.re.kr>
2023-07-28 17:29:21 +0900

    인자 없이 작동하도록 수정

--- group_setting.sh
+++ group_setting.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-re='^[0-9]$'
-if [ $# -ne 1 ] ; then
+if [ $# -ne 0 ] ; then
   echo "Wrong argument" >&2
   exit 1
-elif ! [[ $1 =~ $re ]]; then
-    echo "Error : $1 is not a number" >&2
-    exit 2
 else
-  echo "Group number is $1"
-  sed -i "s/node\([0-9]\).g[0-9].23.gsdc.org/node\1.g$1.23.gsdc.org/g" ansible_setup/inventory/condor_hosts  
-  sed -i "s/node\([0-9]\).g[0-9].23.gsdc.org/node\1.g$1.23.gsdc.org/g" ansible_setup/vars.yml  
-  sed -i "s/node\([0-9]\).g[0-9].23.gsdc.org/node\1.g$1.23.gsdc.org/g" tutorials/chapter4_Advanced/4-1/myinfo.jds
+  GRPNUM=$(hostname | cut -d. -f2 | sed "s/g\([0-9]\)/\1/g")
+  echo "Group number is ${GRPNUM}"
+  sed -i "s/node\([0-9]\).g[0-9].23.gsdc.org/node\1.g${GRPNUM}.23.gsdc.org/g" ansible_setup/inventory/condor_hosts  
+  sed -i "s/node\([0-9]\).g[0-9].23.gsdc.org/node\1.g${GRPNUM}.23.gsdc.org/g" ansible_setup/vars.yml  
+  sed -i "s/node\([0-9]\).g[0-9].23.gsdc.org/node\1.g${GRPNUM}.23.gsdc.org/g" tutorials/chapter4_Advanced/4-1/myinfo.jds
  
   if [[ $(cat ansible_setup/vars.yml | grep nfs | wc -w) -ne 2 ]];
   then
      exit 0
   fi
 
-  if [[ $1 -le 3 ]]; then
+  if [[ ${GRPNUM}1 -le 3 ]]; then
     echo "school gateway 1"
     sed -i "s/nfs_server :/nfs_server : \"10.10.10.226\"/g" ansible_setup/vars.yml
   else
